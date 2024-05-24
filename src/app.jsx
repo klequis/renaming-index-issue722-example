bea4e243fdbddf8484b3bda29d84fa74032260bb
klequis <klequis@gmail.com>
2024-05-24 12:07:39 -0700

    u

--- src/app.jsx
+++ src/app.jsx
@@ -10,6 +10,9 @@ export default function App() {
       root={(props) => (
         <main class="component">
           <div class="file-name">app.jsx</div>
+          <div class="component-subtitle">Example project for issue #722</div>
+          <p>Click on <code>`/blog`</code>, which uses the method of putting the file name in parens: <code>`(blog).jsx`</code>, works.</p>
+          <p>Click on <code>`/socials`</code>, which uses the method of naming the file the same as the folder <code>`socials.jsx`</code>, does not work. It routes to the 404 page.. </p>
           <Nav />
           <Suspense>{props.children}</Suspense>
         </main>
